f45f4302a390ca76eccb6907accae9c2901b8a25
zbx <1213597529@qq.com>
2020-07-27 14:23:56 +0800

    test5

--- src/main/java/com/netty/demo/echo/EchoServer.java
+++ src/main/java/com/netty/demo/echo/EchoServer.java
@@ -17,7 +17,7 @@ public class EchoServer {
 	public static void main(String[] args) throws InterruptedException {
 		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
 	    EventLoopGroup workerGroup = new NioEventLoopGroup();
-//	    final EchoServerHandler serverHandler = new EchoServerHandler();343
+//	    final EchoServerHandler serverHandler = new EchoServerHandler();34345
 	    final MyEchoServerHandler serverHandler = new MyEchoServerHandler();
 		try {
             ServerBootstrap b = new ServerBootstrap();
