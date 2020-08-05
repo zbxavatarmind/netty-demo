82c61f541853ac8e00beb49da7f057b43a2fe019
zbx <1213597529@qq.com>
2020-08-05 11:27:30 +0800

    test11

--- src/main/java/com/netty/demo/echo/EchoClient.java
+++ src/main/java/com/netty/demo/echo/EchoClient.java
@@ -16,6 +16,7 @@ import io.netty.handler.logging.LoggingHandler;
 public class EchoClient {
 	public static void main(String[] args) throws InterruptedException {
 		 // Configure the client.3223344   ----32456EEE
+		//123
         EventLoopGroup group = new NioEventLoopGroup();
         try {
             Bootstrap b = new Bootstrap();
