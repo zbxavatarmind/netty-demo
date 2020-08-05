343603782c93474fc7c086e4614f6f646bb662fe
zbx <1213597529@qq.com>
2020-08-05 14:37:01 +0800

    test16

--- src/main/java/com/netty/demo/echo/EchoServer.java
+++ src/main/java/com/netty/demo/echo/EchoServer.java
@@ -19,7 +19,6 @@ public class EchoServer {
 	    EventLoopGroup workerGroup = new NioEventLoopGroup();
 //	    final EchoServerHandler serverHandler = new EchoServerHandler();567890
 	    //123456
-	    //789
 	    final MyEchoServerHandler serverHandler = new MyEchoServerHandler();
 		try {
             ServerBootstrap b = new ServerBootstrap();
